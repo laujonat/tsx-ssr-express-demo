1ffd2dfcf6fc3f289e3b65ff39637a69ac8ce56f
Jonathan Lau <jon@jonathanlau.io>
2019-06-21 00:50:40 -0700

    syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import { WindowInitData } from './types/index';
+import ReactDOM from "react-dom";
+import { WindowInitData } from "./types/index";
 import App from "./app";
 
 /**
@@ -9,9 +9,6 @@ import App from "./app";
  * It can patch up differences in text content, but you should treat mismatches as bugs and fix them.
  * https://reactjs.org/docs/react-dom.html
  * */
-((data) => {
-    ReactDOM.hydrate(
-    <App data={data} />,
-    document.getElementById("ssr")
-    );
-})((window as WindowInitData).data)
+(data => {
+  ReactDOM.hydrate(<App data={data} />, document.getElementById("ssr"));
+})((window as WindowInitData).data);
